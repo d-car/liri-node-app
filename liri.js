101144fff6c8764c9a920135836197ee4a9e8869
unknown <danie@Dan-Laptop>
2018-08-03 16:30:33 -0500

    omdb funcitonal, formatting to do

--- liri.js
+++ liri.js
@@ -37,6 +37,14 @@ switch(process.argv[2]){
     case "my-tweets":
     myTweets();
     break;
+
+    case "movie-this":
+    if(x){
+      omdbData(x)
+    } else{
+      omdbData("Mr. Nobody")
+    }
+    break;
 }
 
 
@@ -94,3 +102,33 @@ function myTweets() {
             }
     })
 };
+
+function omdbData(movie){
+    var omdbURL = 'http://www.omdbapi.com/?apikey=trilogy&t=' + movie + '&plot=short&tomatoes=true';
+  
+    request(omdbURL, function (error, response, body){
+      if(!error && response.statusCode == 200){
+        var body = JSON.parse(body);
+  
+        console.log("Title: " + body.Title);
+        console.log("Release Year: " + body.Year);
+        console.log("IMdB Rating: " + body.imdbRating);
+        console.log("Country: " + body.Country);
+        console.log("Language: " + body.Language);
+        console.log("Plot: " + body.Plot);
+        console.log("Actors: " + body.Actors);
+        console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
+        console.log("Rotten Tomatoes URL: " + body.tomatoURL);
+  
+      } else{
+        console.log("Error occurred: " + error)
+      }
+      if(movie === "Mr. Nobody"){
+        console.log("-----------------------");
+        console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+        console.log("It's on Netflix!");
+
+      }
+    });
+  
+  }
